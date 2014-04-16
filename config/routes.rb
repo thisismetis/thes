e3c458aafbddd27ee9dab087e7d6aecb7e55aa17
Alex Fox <alexdysonfox@gmail.com>
2014-04-16 09:53:35 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Thes::Application.routes.draw do
   devise_for :users
 
+  root :to => 'homes#show'
+
   resources 'talents', only: [:show]
 
   resources :linkedin
   get 'linkedin_profile' => 'linkedin#linkedin_profile'
   get 'oauth_account' => 'linkedin#oauth_account'
   get 'linkedin_oauth_url' => 'linkedin#generate_linkedin_oauth_url'
-  root :to => 'linkedin#index'
 end
