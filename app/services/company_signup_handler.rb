50e24dec32ea6a5b4c7b1dfb1e714c8f0e239ada
Avi Gilligan <euraldius@gmail.com>
2014-04-18 15:01:17 -0400

    Make CompanyLocation model and migration

--- app/services/company_signup_handler.rb
+++ app/services/company_signup_handler.rb
@@ -10,6 +10,8 @@ class CompanySignupHandler
     Rails.logger.debug company_details
     company = make_user company_details
     make_oauth company
+    company_details["industries"] = prepare_industries company_details["industries"]["all"]
+    company_details["locations"] = prepare_locations company_details["locations"]["all"]
     profile = make_profile(company, company_details)
     company
   end
@@ -17,6 +19,14 @@ class CompanySignupHandler
   private
 
   attr_reader :client
+
+  def prepare_locations(locations)
+    locations.map { |location| Location.create(postal_code: location["postal_code"]) }
+  end
+
+  def prepare_industries(industries)
+    industries.map { |industry| Industry.find_or_create_by(linkedin_id: industry["code"], name: industry["name"]) }
+  end
 
   def make_user(details)
     User.create(email: details["email_address"], type: COMPANY)
@@ -30,12 +40,13 @@ class CompanySignupHandler
   end
 
   def make_profile(company, details)
+    Rails.logger.debug details.inspect
     CompanyProfile.create(
       id: details["id"],
       name: details["name"],
       industries: details["industries"],
       logo_url: details["logo_url"],
-      employee_count_range: details["employee_count_range"],
+      employee_count_range: details["employee_count_range"]["name"],
       locations: details["locations"],
       description: details["description"],
       company: company
---
+++ spec/models/company_location_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe CompanyLocation do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
