2c278a99fb405c3ef430c72f21bc5e6beb5feb58
Avi Gilligan <euraldius@gmail.com>
2014-04-21 16:23:34 -0400

    Build Talent ability to edit Proficiency level

--- app/controllers/proficiencies_controller.rb
+++ app/controllers/proficiencies_controller.rb
@@ -1,6 +1,27 @@
 class ProficienciesController < ApplicationController
   def edit
-    @talent = Talent.find(params[:talent_id])
+    @talent = find_talent
     @proficiencies = @talent.profile.proficiencies
+  end
+
+  def update
+    talent = find_talent
+    talent.update(proficiencies_params)
+    redirect_to talent
+  end
+
+  private
+
+  def find_talent
+    Talent.find(params[:talent_id])
+  end
+
+  def proficiencies_params
+    params.require(:talent).permit(
+      profile_attributes: [
+        :id,
+        proficiencies_attributes: [:id, :level]
+      ]
+    )
   end
 end
--- app/models/talent_profile.rb
+++ app/models/talent_profile.rb
@@ -2,4 +2,6 @@ class TalentProfile < ActiveRecord::Base
   belongs_to :talent
   has_many :proficiencies
   has_many :skills, through: :proficiencies
+
+  accepts_nested_attributes_for :proficiencies
 end
