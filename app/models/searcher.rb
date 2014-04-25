7616fc30d4b08ed1631f5c1f7729949931d7208a
Avi Gilligan <euraldius@gmail.com>
2014-04-25 00:38:46 -0400

    Allow for searching on multiple skills

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -1,7 +1,7 @@
 class Searcher
   def initialize(search_params)
-    @query = search_params[:query]
-    @level = search_params[:level]
+    @skill_names = search_params[:query]
+    @levels = search_params[:level]
   end
 
   def profiles
@@ -10,15 +10,46 @@ class Searcher
 
   private
 
-  def talent_profile_ids
-    proficiencies.map(&:talent_profile_id).uniq
-  end
+  attr_reader :skill_names, :levels, :proficiencies
 
-  def skills
-    Skill.includes(:talent_profiles).where("name ILIKE :query", query: "%#{@query}%")
+  def talent_profile_ids
+    ids = proficiencies.map(&:talent_profile_id)
+    ids.select { |id| ids.count(id) == skill_names.count }
   end
 
   def proficiencies
-    Proficiency.where(level: @level, skill_id: skills)
+    @proficiencies ||= Proficiency.joins(:skill).where(where_clause)
+  end
+
+  def where_clause
+    wheres.join(" OR ")
+  end
+
+  def wheres
+    wheres = []
+    skill_names.each do |index, skill_name|
+      unless skill_name.empty?
+        wheres << build_where(skill_name, levels[index])
+      end
+    end
+    wheres
+  end
+
+  def build_where(skill_name, level)
+    "(#{skill_where(skill_name)} AND #{level_where(level)})"
+  end
+
+  def skill_where(skill_name)
+    skill_name = sanitize("%#{skill_name}%")
+    "skills.name ILIKE #{skill_name}"
+  end
+
+  def level_where(level)
+    level = sanitize(level)
+    "proficiencies.level = #{level}"
+  end
+
+  def sanitize(term)
+    ActiveRecord::Base.sanitize(term.to_s)
   end
 end
